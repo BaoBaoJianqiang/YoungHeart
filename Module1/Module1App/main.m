d219caeea43ce4d9804d3a9337ae3e3c636a30d1
jianqiang.bao <jianqiang.bao@qunar.com>
2015-12-09 18:44:56 +0800

    新建AppModule1，Module1已经可以单独运行并调试了

---
+++ Module1/Module1App/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Module1App
+//
+//  Created by baojianqiang on 15/12/9.
+//  Copyright © 2015年 baojianqiang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
